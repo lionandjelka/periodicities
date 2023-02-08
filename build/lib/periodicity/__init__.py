07ab4480ea76b54077a6180b38cc25e1eb4e1d1d
lionandjelka <angelkovacevic@gmail.com>
2023-02-08 16:55:48 +0100

    Update __init__.py

--- build/lib/periodicity/__init__.py
+++ build/lib/periodicity/__init__.py
@@ -2,6 +2,6 @@
 Python interface to SER-SAG in-kind LSST proposal periodicity module
 """
 #from .read import *
-#from .plots import *
+from .plots import *
 from .algorithms import *
 from .utils import *
